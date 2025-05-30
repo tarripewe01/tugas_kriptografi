a1972265a1040b2d157e7724ed102bbd2af2eaf5
tarripewe <tarri@karadigital.co>
2025-05-30 15:34:01 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugas_kriptografi',
+  webDir: 'dist'
+};
+
+export default config;
